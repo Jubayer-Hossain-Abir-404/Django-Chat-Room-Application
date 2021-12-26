58b62f05f487b3cfe48d020b1133dbf2dd49e6a6
Jubayer-Hossain-Abir-404 <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2021-12-26 07:33:11 +0600

    Update

--- chatroom/chatroom/urls.py
+++ chatroom/chatroom/urls.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+#request object is going to be like the http object
+def home(request):
+    return HttpResponse ('Home Page')
+      
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
