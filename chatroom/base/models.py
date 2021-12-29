18ab8a2951cdb2e746dd16f89cf3848b75c3a9e0
Jubayer-Hossain-Abir-404 <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2021-12-29 21:29:12 +0600

    Update

--- chatroom/base/models.py
+++ chatroom/base/models.py
@@ -2,8 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-# class Room(models.Model):
+class Room(models.Model):
     # host = 
-    #topic =
-    # name= models.CharField(max_length=200)
-    # description = models.TextField(null=True)
+    # topic =
+    name= models.CharField(max_length=200) # Meaning this can't be blank
+                                           # for that null is false
+    description = models.TextField(null=True, blank=True) 
+    # here textfield true. can be blank
+    # it means when form is submitted description can be blank
+    # participants = 
+    # all the participants will be kept in there
+    updated = models.DateTimeField(auto_now=True) 
+    # this will change everytime instance is saved 
+    created = models.DateTimeField(auto_now_add=True)
+    # this will be set at the very begining and never change again
+
+
+    def __str__(self):
+        return self.name  #This will return name
+
