38bb50cf6b1b63e17ff9d56a4cbf59e384a2acb3
Jubayer-Hossain-Abir-404 <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-01-01 09:59:39 +0600

    Update

--- chatroom/base/models.py
+++ chatroom/base/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User # built in way of creating a user
 
 # Create your models here.
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
 class Room(models.Model):
-    # host = 
-    # topic =
+    # Somebody has to host a room and then a topic
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # It needs to be allowed set null=True
+    # Because when the parent table gets removed and set to null it should 
+    # be made sure that the database will allow it
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name= models.CharField(max_length=200) # Meaning this can't be blank
                                            # for that null is false
     description = models.TextField(null=True, blank=True) 
@@ -20,4 +31,21 @@ class Room(models.Model):
 
     def __str__(self):
         return self.name  #This will return name
+
+
+class Message(models.Model):
+    # This is going to be a one to many relationship
+    # user can have many messages
+    # whereas messages can have one user
+     user = models.ForeignKey(User, on_delete=models.CASCADE)
+     room = models.ForeignKey(Room, on_delete=models.CASCADE)
+     body = models.TextField()
+     updated = models.DateTimeField(auto_now=True) 
+     created = models.DateTimeField(auto_now_add=True)
+
+
+     def __str__(self):
+         return self.body[0:50] # for the first 50 characters
+
+
 
--- chatroom/base/views.py
+++ chatroom/base/views.py
@@ -4,24 +4,27 @@ from .models import Room
 
 # Create your views here.
 
-rooms1 = [
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend Developers'},
-]
+# rooms1 = [
+#     {'id': 1, 'name': 'Lets learn python'},
+#     {'id': 2, 'name': 'Design with me'},
+#     {'id': 3, 'name': 'Frontend Developers'},
+# ]
 
 
 #request object is going to be like the http object
 # return HttpResponse ('Home Page') # this returns html code directly
 def home(request):
+    rooms1 = Room.objects.all() #this will give all the room in the database
     context = {'rooms': rooms1}
     return render(request, 'base/home.html', context) 
 
 def rooms(request, pk):
     # return HttpResponse('Room')
-    room = None
-    for i in rooms1:
-        if i['id'] == int(pk):
-            room = i
+    # room = None
+    # for i in rooms1:
+    #     if i['id'] == int(pk):
+    #         room = i
+
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
