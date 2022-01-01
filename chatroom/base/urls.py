d7ceeb88159e2fa936da6c7c18fed3427e754f81
Jubayer-Hossain-Abir-404 <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-01-01 10:30:53 +0600

    Update

--- chatroom/base/urls.py
+++ chatroom/base/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),  # here keeping name="home" is a good practice
     path('room/<str:pk>/', views.rooms, name="rooms"), # calling rooms function from views
+
+    path('create-room/', views.createRoom, name="create-room"),
     
 ]
