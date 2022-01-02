1cfa3fd0cfcb24c1139905962bf3d6a2851d630d
Jubayer-Hossain-Abir-404 <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-01-02 07:39:57 +0600

    Update

--- chatroom/base/views.py
+++ chatroom/base/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -34,7 +35,7 @@ def loginpage(request):
 
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -64,9 +65,21 @@ def logoutUser(request):
 
 
 def registerPage(request):
-    page = 'register'
-    context ={'page': page}
-    return render(request, 'base/login_register.html', context)
+    # page = 'register'
+    # Django provided user creation form
+    form = UserCreationForm()
+    # context ={'page': page}
+    if request.method =='POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+    return render(request, 'base/login_register.html', {'form': form})
 
 def home(request):
     # inline if statement
