ea3b493074ee11e5ecbe4ccdce6092d3fe28fe3b
Jubayer-Hossain-Abir-404 <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-01-05 07:29:57 +0600

    Update

--- chatroom/chatroom/settings.py
+++ chatroom/chatroom/settings.py
@@ -130,6 +130,10 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 
+# There is a different way to upload user defined images
+# and that's why static_root is required
+# STATIC_ROOT = 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
