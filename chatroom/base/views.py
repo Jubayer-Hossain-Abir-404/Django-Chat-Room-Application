0a9536858a448a0360986d1a71d9205899f7d2bc
Md. Jubayer Hossain Abir <41603187+Jubayer-Hossain-Abir-404@users.noreply.github.com>
2022-02-25 22:54:36 +0600

    Update

--- chatroom/base/views.py
+++ chatroom/base/views.py
@@ -287,5 +287,7 @@ def updateUser(request):
 
 
 def topicsPage(request):
-    return render(request, 'base/topics.html', {})
+    q= request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics':topics})
 
